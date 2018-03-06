229f85030d5dcea617240e0f4a764a3c19e76224
smithwebtek <brad@smithwebtek.com>
2018-03-05 20:43:26 -0500

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,7 +55,7 @@ end
 def runner
   welcome 
   card_total = initial_round
-  until card_total > 20
+  until card_total > 21
 		card_total += hit?(card_total)
 		display_card_total(card_total)
 	end
